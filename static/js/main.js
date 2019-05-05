ad57c4a0e17a911b1b811b247084fafe8069f798
Dan Tenenbaum <dtenenba@fhcrc.org>
2019-05-05 13:10:56 -0700

    fix input text fields - they now show file name after selection

--- static/js/main.js
+++ static/js/main.js
@@ -108,3 +108,38 @@ $(function () {
     }
 
 });
+
+/////
+
+$(document).ready(function () {
+    console.log("in doc ready function");
+
+    $(':file').attr('placeholder', '');
+
+    $("#file1").bind('change',  function() {
+        console.log("does this work?");
+    });
+    $(':file').bind('change', function() {
+        console.log("in change event handler");
+        var assoc = $(this).attr('assoc');
+        console.log("assoc is " + assoc);
+
+        var arr = $(this).val().split("\\");
+        var name = arr[arr.length -1];
+        $("#" + assoc).attr('placeholder', name);
+    });
+
+    $(':file').on('fileselect', function (event, numFiles, label) {
+        console.log("fileselect triggered");
+        var input = $(this).parents('.input-group').find(':text'),
+            log = numFiles > 1 ? numFiles + ' files selected' : label;
+
+        if (input.length) {
+            input.val(log);
+        } else {
+            if (log) alert(log);
+        }
+
+    });
+});
+
