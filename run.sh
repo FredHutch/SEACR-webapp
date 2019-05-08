cf6184b8889151037263d551fb7526c90264bdf4
Dan Tenenbaum <dtenenba@fhcrc.org>
2019-05-08 10:23:28 -0700

    remove everything except index.md from gh-pages branch

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-APP_SECRET=$(cat /run/secrets/BATCH_DASHBOARD_APP_SECRET)
-export APP_SECRET
-
-VENV=$(pipenv --venv)
-
-"$VENV/bin/gunicorn" app:APP --timeout 120 -w 4 -b 0.0.0.0:8001
